aa2205de3f4a0ff1b052d5bc6066cd8c85abe173
OfficialCRUGG <ich@crugg.de>
2020-02-04 18:38:36 +0100

    :ambulance: Temporairly disabled API Examples

--- pages/examples/hug.vue
+++ pages/examples/hug.vue
@@ -4,7 +4,7 @@
       <img src="/assets/logo.png" width="100" height="100" />
       <h1>AniGifs</h1>
       <h2>{{ $t("examples") }} - {{ $t("example.hug") }}</h2>
-      <img :src="image" max-height="500" max-width="700" /><br>
+      <img src="/assets/apinotup.png" max-height="500" max-width="700" /><br>
       <v-btn to="/">{{ $t("home") }}</v-btn>
     </v-container>
   </div>
@@ -19,9 +19,11 @@
 import axios from "axios";
 
 export default {
+  /*
   async asyncData() {
     let image = (await axios.get("https://api.taigabot.net/v2/anigifs/hug")).data.image
     return { image };
   }
+  */
 }
 </script>
